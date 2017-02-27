055f5601166ba546f0df918d91c16a0bd08e5f31
Landis1013 <bill.landis1013@gmail.com>
2017-02-27 15:36:25 -0500

    Master

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -11,20 +11,34 @@ namespace Vidly.Controllers
 	public class CustomersController : Controller
 	{
 
+		// -- Get Customers from Database --
+		private ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+		// -- Get Customers from Database --
+
+
+
 		// GET: /Customers/
 		public ViewResult Index()
 		{
-			var customers = GetCustomers();
-
+			var customers = _context.Customers.ToList();
+			// var customers = GetCustomers();
 			return View(customers);
 		}
-
 
 		// GET: /Customers/
 		public ActionResult Details(int id)
 		{
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
-
+			var customer = _context.Customers.ToList().SingleOrDefault(c => c.Id == id);
+			// var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
 			if (customer == null)
 				return HttpNotFound();
 
@@ -32,14 +46,15 @@ namespace Vidly.Controllers
 		}
 
 
-		private IEnumerable<Customer> GetCustomers()
-		{
-			return new List<Customer>
-			 {
-				 new Customer { Id = 1, Name = "John Smith" },
-				 new Customer { Id = 2, Name = "Mary Williams" }
-			 };
-		}
+		// -- Get Customers from Database --
+		//private IEnumerable<Customer> GetCustomers()
+		//{
+		//    return new List<Customer>
+		//     {
+		//         new Customer { Id = 1, Name = "John Smith" },
+		//         new Customer { Id = 2, Name = "Mary Williams" }
+		//     };
+		//}
 
 	}
 }
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
 using System.Web;
 
 namespace Vidly.Models
 {
-    // bl
     public class MembershipType
     {
         public byte Id { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
-
-        public string test { get; set; }
 
     }
 }
